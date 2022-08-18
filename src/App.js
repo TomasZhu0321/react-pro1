0615feb39fcb2e279daa8a419e9ef16dd440bf7c
ThomasZhu <tomaszhu0321@gmail.com>
2022-08-18 10:57:49 -0400

    child->parent

--- src/App.js
+++ src/App.js
@@ -22,10 +22,15 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+
   ];
+    const addExpenseHandler = expense =>{
+      console.log('In App.js');
+      console.log(expense);
+    }
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}></Expenses>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,17 @@ import React from "react"
 import "./NewExpense.css";
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = ()=>{ 
+const NewExpense = (props)=>{ 
+    const saveExpenseHandler = (eneterExpenseData)=>{
+        const expenseData ={
+            ... eneterExpenseData,
+            id : Math.random().toString()
+        }
+        props.onAddExpense(expenseData);
+}
     return (
         <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseHandler}/>
         </div>
     )
  }
